526e8ae0c1adbb5a72b907ea0c69b9eb29064362
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-03-27 21:00:06 -0300

    forgotPass

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,25 +1,73 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View,TextInput,TouchableOpacity, Pressable } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity, Pressable ,Image} from 'react-native';
 
 export default function ForgotPassword() {
     return(
         <View style={styles.container}
         >
-            <Text style={
-                {
-                    alignSelf:"center",
-                    justifyContent: "center",
-                    padding:100,
-                    fontSize:30,
-                }
-            }>Forgot Password</Text>
+            <View style={styles.topView}/>
+            <Image source={require('../assets/forgotPass.png')}
+                   style={styles.image}/>
+            <Text style={styles.tittle}>Forgot your password? </Text>
+            <Text style={styles.subtitle}>Press "send" to reciebe an email and recover your password.</Text>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.send}>SEND</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:"#6495ed",
+        backgroundColor:"#fff",
         flex:1,
     },
+
+    send:{
+        fontSize:20,
+        justifyContent:"center",
+        alignSelf:"center",
+    },
+
+    topView:{
+        flex:0.2
+    },
+
+    tittle:{
+        fontSize:30,
+        fontWeight:"bold",
+        justifyContent:"center",
+        alignSelf:"center",
+        marginTop:30,
+
+    },
+
+    subtitle:{
+        fontSize:25,
+        fontWeight:"300",
+        justifyContent:"center",
+        alignSelf:"center",
+        marginTop:30,
+    },
+
+    image:{
+        height:100,
+        width:100,
+        marginTop:70,
+        alignSelf:"center",
+        flex:0.24,
+    },
+
+    button:{
+        alignItems: "center",
+        backgroundColor: "#9acd32",
+        padding: "5%",
+        alignSelf:"center",
+        width:"40%",
+        marginTop:50,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius:20,
+        borderBottomRightRadius:20,
+    }
 });
