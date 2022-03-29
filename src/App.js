9af4b103569675637d69b13aaa05e8ddc085f30d
Fede <federico.pochat@ing.austral.edu.ar>
2022-03-29 00:10:51 -0300

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     //const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-        <ForgotPasswordScreen/>
+        <TabBarNavigator/>
         {/*
         <RootStackScreen/>
         <Stack.Navigator screenOptions={{
