b72ee9f0f2b107ca403d720adbce4568ca5e9a9b
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-03-29 19:32:04 -0300

    forgotPass

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,88 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Dimensions, Image, Button, TouchableOpacity,StatusBar,Platform,TextInput, ImageBackground} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
+import * as Animatable from 'react-native-animatable';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
-function ProfileScreen(props) {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+export default function ForgotPasswordScreen() {
+
+    const [activate, setActivate]= useState(true);
+
+    const toggleBell = ()=>{
+        setActivate(!activate);
+    }
+    return(
+        <Animatable.View
+            animation="fadeInUpBig" style={styles.container}>
+            <View style={styles.horizontal}>
+                <Text style={styles.tittle}>GolfTrack</Text>
+                  <TouchableOpacity style={styles.bell} onPress={toggleBell}>
+                      {activate?
+                       <Feather
+                           name="bell"
+                           color="#05375a"
+                           size={30}
+                       />:
+                          <Feather
+                              name="bell-off"
+                              color="#05375a"
+                              size={30}
+                          />}
+                  </TouchableOpacity>
+            </View>
+            <View style={styles.picture}>
+                <Feather
+                    name="user"
+                    color="#05375a"
+                    size={100}
+                    style={styles.image}
+                />
+                <Text style={styles.username}>Username</Text>
+            </View>
+        </Animatable.View>
     );
 }
 
-export default ProfileScreen;
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+
+    tittle:{
+        fontSize:30,
+        fontWeight:"bold",
+        color: '#05375a',
+        marginLeft:10,
+    },
+
+    horizontal:{
+        marginTop:10,
+        flexDirection: 'row',
+    },
+    bell: {
+        alignSelf:"flex-end",
+        marginLeft:220,
+    },
+
+    picture:{
+        flex:2,
+        alignContent:"center",
+    },
+
+    image:{
+        alignSelf:"center",
+        marginTop:10,
+    },
+
+    username:{
+        alignSelf:"center",
+        fontSize:25,
+        fontWeight:"bold",
+        color: '#05375a',
+        marginLeft:10,
+    }
+
+
+});
+
