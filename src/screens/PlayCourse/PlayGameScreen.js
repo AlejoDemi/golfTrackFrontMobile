23db5749d3b8b22e8cc4911f10305b813f61f5dd
Fede <federico.pochat@ing.austral.edu.ar>
2022-05-18 18:21:24 -0300

    Imporved a lot the map

--- src/screens/PlayCourse/PlayGameScreen.js
+++ src/screens/PlayCourse/PlayGameScreen.js
@@ -1,39 +1,34 @@
-import * as React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import {useSelector} from "react-redux";
 import {createRef, useEffect, useState} from "react";
+import PlayingHole from "./PlayingHole";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faAngleRight, faAngleLeft} from "@fortawesome/free-solid-svg-icons";
 
 const PlayGameScreen = () => {
-    const mapView = createRef();
+    const [holeNum, setHoleNum] = useState(1);
     const course = useSelector(state => state.course);
-    console.log(course);
 
-    useEffect(() => {
-        mapView.current.fitToSuppliedMarkers(['Teebox','Green'])
-    },[])
-
-    return (
+    return(
         <View style={styles.container}>
-            <MapView
-                ref={mapView}
-                scrollEnabled={false}
-                rotateEnabled={false}
-                style={styles.map}
-                mapType="satellite"
-            >
-                <Marker
-                    identifier={'Teebox'}
-                    centerOffset={{x: 0.5, y: 5}}
-                    coordinate={{ latitude : course.course.holesList[0].locationTeebox.lat, longitude : course.course.holesList[0].locationTeebox.lng }}
-                />
-                <Marker
-                    identifier={'Green'}
-                    coordinate={{ latitude : course.course.holesList[0].locationMidOfGreen.lat, longitude : course.course.holesList[0].locationMidOfGreen.lng }}
-                />
-            </MapView>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => setHoleNum(holeNum - 1)}>
+                    <FontAwesomeIcon
+                        icon={faAngleLeft}
+                        size={30}
+                    />
+                </TouchableOpacity>
+                <Text style={styles.holeText}>Hole {holeNum}</Text>
+                <TouchableOpacity onPress={() => setHoleNum(holeNum + 1)}>
+                    <FontAwesomeIcon
+                        icon={faAngleRight}
+                        size={30}
+                    />
+                </TouchableOpacity>
+            </View>
+            <PlayingHole hole={course.course.holesList[holeNum-1]}/>
         </View>
-    );
+    )
 }
 
 export default PlayGameScreen;
@@ -41,12 +36,21 @@ export default PlayGameScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+    },
+    header: {
+        flexDirection:'row',
+        elevation: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        zIndex: 1,
+        justifyContent: 'space-around',
+        height: '15%',
+        backgroundColor: '#fff',
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
     },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    },
-});
+
+    holeText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
+})
---
+++ src/screens/PlayCourse/PutScoreScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const PutScoreScreen = (props) => {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default PutScoreScreen;
